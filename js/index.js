ff81b829586052dde2e7bda210587113fa335403
Ryan Bennek <rbennek.rb@gmail.com>
2023-12-27 15:14:00 -0600

    Implement return data function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//TODO - Build function to return lat/lon from Geolocated area 
 
 //Function to build URL for API
 function buildCall(lat,lon) {
@@ -5,21 +6,24 @@ function buildCall(lat,lon) {
 const appid = 'appid=ae61075132cbaa8fb984d546e6783f50';
 var lat = `lat=${lat}`;
 var lon = `lon=${lon}`;
-callBase = 'https://api.openweathermap.org/data/2.5/weather?';
+var callBase = 'https://api.openweathermap.org/data/2.5/weather?';
 //Build URL
-return callURL = `${callBase}${lat}&${lon}&${appid}`
+var callURL = `${callBase}${lat}&${lon}&${appid}`
+return (callURL);
 }
 // * https://api.openweathermap.org/data/2.5/weather?lat=90&lon=90&appid={ID}
 const apiCall = buildCall(90,90);
 
-const getWeather = async () => {
-  const endpoint = apiCall;;
+// Return 
+  async function getWeather()  {
+  const endpoint = apiCall;
   console.log(`Generating weather data`)
   try {
     const response = await fetch(endpoint, {cache: 'no-cache'});
     if (response.ok) {
       const jsonResponse = await response.json();
-      console.log(jsonResponse);
+      return jsonResponse;
+       
     }
   }
   catch(error) {
@@ -27,7 +31,12 @@ const getWeather = async () => {
   }
 }
 
-getWeather();
+let weatherData = await getWeather();
+
+console.log(weatherData.main.temp);
+
+
+
 
 
 

361fede56eb5a3d25dec35a28965f8a12a269dff
Ryan Bennek <rbennek.rb@gmail.com>
2024-01-02 07:50:03 -0600

    Implement Fragment Builder

--- js/helpers.js
+++ js/helpers.js
@@ -1,7 +1,7 @@
 export { sanitizeData, displayData };
 import { getData } from "./api.js";
 
-// * Bundles data into usable object
+// * Remove unused data, returns an object
 function sanitizeData(weatherData) {
   let dataObject = {};
 
@@ -27,13 +27,14 @@ function sanitizeData(weatherData) {
   const visibility = weatherData.visibility;
   dataObject.visibility = visibility;
 
+  // todo - can we normalize this data with a .floor? Get rid of decimal.
   // Return object
 
   return dataObject;
 }
 
 //* Displays data
-async function displayData() {
+async function displayData(object) {
   // Get data from API and set to weatherData
   let weatherData = await getData();
   // catch error
@@ -44,4 +45,24 @@ async function displayData() {
   // Returns an object with only the needed data
   weatherData = sanitizeData(weatherData);
   console.log(weatherData);
+
+  //New HTML Fragment, target HTML element we're going to append it to
+  const fragment = new DocumentFragment();
+  const targetElement = document.getElementById("results-container");
+
+  //Build "Title" of weather fragment
+  const title = document.createElement("span");
+  title.classList.add("card-title");
+  title.setAttribute("id", "results-title");
+  title.textContent = `Weather conditions in ${weatherData.name}:`;
+  fragment.appendChild(title);
+  // Build "Temp" of weather fragment
+
+  const currentTemp = document.createElement("span");
+  currentTemp.setAttribute("id", "temp");
+  currentTemp.textContent = `Currently, the temperature is ${weatherData.mainWeather.temp}°F. It feels like ${weatherData.mainWeather.feels_like}°F. `;
+  fragment.appendChild(currentTemp);
+
+  // Append Fragment to results card
+  targetElement.appendChild(fragment);
 }
